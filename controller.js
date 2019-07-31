04389a4f5cc2c3fcd2aece1ed697cb9d2ec6b266
KiJun Park <ekdlwns3685@naver.com>
2019-07-31 14:41:23 +0900

    added add words

--- controller.js
+++ controller.js
@@ -1,3 +1,5 @@
+import alert from "alert-node";
+import Words from "./model/Words";
 import routes from "./routes";
 import db from "./db";
 
@@ -5,19 +7,33 @@ export const home = (req, res) => {
   res.render("home", { pageTitle: "F word generator" });
 };
 
-export const generateWords = (req, res) => {
-  // const num = Math.floor(Math.random() * db.length);
-  // res.render("home", {
-  //   words: db[num].word
-  // });
+export const generateWords = async (req, res) => {
+  try {
+    const terms = await Words.find({});
+    const num = Math.floor(Math.random() * terms.length);
+    const words = terms[num].words;
+
+    res.render("home", { pageTitle: "F word generator", words });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const getAddWords = (req, res) => {
   res.render("addWords", { pageTitle: "addWords" });
 };
 
-export const postAddWords = (req, res) => {
+export const postAddWords = async (req, res) => {
   console.log(req.body);
+  const {
+    body: { addWords }
+  } = req;
+
+  const newWords = await Words.create({
+    words: addWords
+  });
+  alert("Thank you for adding the word.");
+  res.redirect(routes.home);
 };
 
 export const postReport = (req, res) => {
